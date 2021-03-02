80470a8e86a2bc800bc36ecab2500b68b7213809
Lagashettik <Lagashettik@gmail.com>
2021-03-02 21:11:24 +0530

    Created simple linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,5 +1,28 @@
-public class LinkedList {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class LinkedList {
+    Node head = null;
+    Node tail = null;
+    Scanner scan = new Scanner(System.in);
+
+    void add(int data){
+        Node node = new Node(data);
+        if(tail != null){
+            tail.next = node;
+            tail = node;
+        }
+        else{
+            head = node;
+            tail = node;
+        }
     }
+
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(56);
+        linkedList.add(30);
+        linkedList.add(70);
+    }
+
 }
